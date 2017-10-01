2305a0ab33b5feb18f1f07f57ec4d30269cbd063
c0deX1 <sizv1996@gmail.com>
2017-10-02 03:43:37 +0700

    Add some code

---
+++ Regression/LinearRegression.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regression
+{
+    public class LinearRegression
+    {
+        public double[] valX, valY, ost;
+        public double standardXdev, standardYdev, correlation, slope, interception;
+
+        public LinearRegression(double[] valX, double[] valY)
+        {
+            this.valX = valX;
+            this.valY = valY;
+            standardXdev = GetStandDev(valX);
+            standardYdev = GetStandDev(valY);
+            correlation = GetCorrelation(valX, valY);
+            slope = correlation * standardYdev/ standardXdev;
+            interception = GetMean(valY) - slope * GetMean(valX);
+        }
+
+        public static double GetMean(double[] array)
+        {
+            return array.Sum() / array.Length;
+        }
+
+        public static double GetStandDev(double[] array)
+        {
+            double mean = GetMean(array);
+            double[] meanDev = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                meanDev[i] = Math.Pow(array[i] - mean, 2);
+            return Math.Sqrt(meanDev.Sum()/(meanDev.Length - 1));
+        }
+        private static double GetCorrelation(double[] X, double[] Y)
+        {
+            double XMean = X.Sum() / X.Length;
+            double YMean = Y.Sum() / Y.Length;
+            double[] x = new double[X.Length];
+            double[] y = new double[Y.Length];
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                x[i] = X[i] - XMean;
+                y[i] = Y[i] - YMean;
+            }
+
+            double[] xy = new double[X.Length];
+            for (int i = 0; i < X.Length; i++)
+            {
+                xy[i] = x[i] * y[i];
+            }
+
+            double[] xPowed = new double[X.Length];
+            double[] yPowed = new double[Y.Length];
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                xPowed[i] = Math.Pow(x[i], 2);
+                yPowed[i] = Math.Pow(y[i], 2);
+            }
+
+            return xy.Sum() / Math.Sqrt(xPowed.Sum() * yPowed.Sum());
+        }
+
+        public double Predict(double x)
+        {
+            return interception + slope * x;
+        }
+
+        public string GetEquation()
+        {
+            return Math.Round(this.slope,6) + "*x + " + Math.Round(this.interception, 6);
+        }
+        public void GetOstatok(){
+            ost = new double[valY.Length];
+            for (int i = 0; i < valY.Length; i++)
+                this.ost[i] = valY[i] - (interception + slope * valX[i]);
+
+        }
+        public double GetDetermination()
+        {
+            GetOstatok();
+            double Qe = 0;
+            for (int i=0; i<ost.Length; i++)
+                 Qe+=Math.Pow(ost[i],2);
+            double Qy = 0;
+            for (int i = 0; i < valY.Length; i++)
+                Qy += Math.Pow(valY[i] - GetMean(valY), 2);    
+            return Math.Round(1 - (Qe/Qy), 6);
+        }
+    }
+}
---
+++ Regression/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Threading.Tasks;
+
+namespace Regression
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+            B2.IsEnabled = false;
+        }
+
+        double[] yLine = new double[300];
+        double[] xLine = new double[300];
+        int rw;
+        int cl;
+        Excel.Range range;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            await Task.Run(() =>
+            {
+                OpenFile();
+            });
+        }
+
+        private void OpenFile()
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(@"C:\Users\rbt1\Documents\Visual Studio 2013\Projects\Regression\Regression\bin\Debug\stat.csv", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+
+            string[] values = new string[(range.Cells[1, 1] as Excel.Range).Value2.ToString().Split(',').Length];
+            for (int i = 0; i < (range.Cells[1, 1] as Excel.Range).Value2.ToString().Split(',').Length; i++)
+                values[i] = (range.Cells[1, 1] as Excel.Range).Value2.ToString().Split(',')[i];
+
+
+                foreach (var value in values)
+                {
+                Dispatcher.Invoke(()=> { comboValues1.Items.Add(value); });
+                Dispatcher.Invoke(() => { comboValues2.Items.Add(value); });
+                Dispatcher.Invoke(() => { B2.IsEnabled = true; });
+                }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int firstItem = comboValues1.SelectedIndex;
+            int secondItem = comboValues2.SelectedIndex;
+            for (int i = 2; i < 302; i++)
+            {
+                // if (Convert.ToDouble((range.Cells[i, 1] as Excel.Range).Value2.ToString().Split(',')[secondItem].CompareTo(".")))
+                yLine[i - 2] = Convert.ToDouble((range.Cells[i, 1] as Excel.Range).Value2.ToString().Split(',')[secondItem]);
+                // if(Convert.ToDouble((range.Cells[i, 1] as Excel.Range).Value2.ToString().Split(',')[firstItem].CompareTo(".")))
+                xLine[i - 2] = Convert.ToDouble((range.Cells[i, 1] as Excel.Range).Value2.ToString().Split(',')[firstItem].Replace('.', ','));
+            }
+            KeyValuePair<double, double>[] valueList = new KeyValuePair<double, double>[xLine.Length];
+
+            LinearRegression LR = new LinearRegression(xLine, yLine);
+            labelEq.Content = LR.GetEquation();
+            labelR.Content = LR.GetDetermination();
+
+            for (int i = 0; i < xLine.Length; i++)
+                valueList[i] = new KeyValuePair<double, double>(xLine[i], yLine[i]);
+
+            KeyValuePair<double, double>[] regressionLine = new KeyValuePair<double, double>[2] { new KeyValuePair<double, double>(xLine.Min(), LR.Predict(xLine.Min())), new KeyValuePair<double, double>(xLine.Max(), LR.Predict(xLine.Max())) };
+
+            ((LineSeries)Graph.Series[0]).ItemsSource = valueList;
+            ((LineSeries)Graph.Series[1]).ItemsSource = regressionLine;
+            Gr2.Title = "Regression Line";
+            Gr1.Title = "Data points";
+        }
+    }
+}
+
